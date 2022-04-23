3701b68b0fb60509ddee311812108a17e41a0091
Stan <stasostaszewski@gmail.com>
2022-04-23 14:57:14 +0200

    Parallax scroller cleanup

---
+++ Assets/Scripts/Effects/DoodadFlicker.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoodadFlicker : MonoBehaviour
+{
+    [SerializeField] bool flicker;
+    [SerializeField] float minDelay;
+    [SerializeField] float maxDelay;
+
+
+}
--- Assets/Scripts/Effects/ParallaxScroller.cs
+++ Assets/Scripts/Effects/ParallaxScroller.cs
@@ -23,16 +23,21 @@ public class ParallaxScroller : MonoBehaviour
         OnScreenCheck();
         CounteractCameraMovement();
     }
-
+    private void OnScreenCheck()
+    {
+        isOnScreen = CameraInformation.IsPositionOnScreen(transform.position);
+    }
     private void CounteractCameraMovement()
     {
+        if (!isOnScreen)
+        {
+            return;
+        }
         Vector2 cameraPosition = mainCamera.transform.position;
-
         Vector2 offset = countParallaxOffset(cameraPosition);
 
         transform.position = startingPosition + offset;
     }
-
     private Vector2 countParallaxOffset(Vector2 cameraPosition)
     {
         float parallaxFactor = (MAX_DISTANCE - distanceFromCamera) / MAX_DISTANCE;
@@ -40,8 +45,4 @@ public class ParallaxScroller : MonoBehaviour
         return new Vector2(cameraPosition.x * parallaxFactor, cameraPosition.y * parallaxFactor);
     }
 
-    private void OnScreenCheck()
-    {
-        //mainCamera.
-    }
 }
