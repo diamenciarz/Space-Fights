69b4b11611866da451875d4e879b3ceb5f2d73c9
Stan <stasostaszewski@gmail.com>
2023-06-13 22:49:43 +0200

    Added better obstacle avoidance for ship controller

--- Assets/Scripts/Instantiable/Ships/Movement/AI/ShipController.cs
+++ Assets/Scripts/Instantiable/Ships/Movement/AI/ShipController.cs
@@ -5,16 +5,22 @@ using static MovementScheme;
 using static StaticDataHolder;
 using static UnityEditor.Progress;
 
-public class ShipController : TeamUpdater
+public class ShipController : TeamUpdater, ISerializationCallbackReceiver
 {
-    [SerializeField] float chaseRange;
-    [SerializeField] float avoidRange;
+    [SerializeField][Tooltip("Will stop chasing its target above that distance")] float chaseRange;
+    [SerializeField][Tooltip("Will start avoiding obstacles below that distance")] float avoidRange;
+    [SerializeField][Tooltip("Will not come closer to target than this distance")] float attackRange;
     [SerializeField] bool isForceGlobal;
+    [SerializeField][Range(0, 1)][Tooltip("0 - obstacles ignored when chasing; 1 - obstacles avoided at close range even when chasing")] float obstacleAvoidance;
 
     IEntityMover myVehicle;
     private Rigidbody2D rb2D;
     public GameObject targetToChase;
     private MovementMode movementMode;
+    private IParent myParent;
+
+    //Random movement
+    private float movementPeriod;
 
     enum MovementMode
     {
@@ -28,21 +34,44 @@ public class ShipController : TeamUpdater
     {
         base.Start();
         SetupStartingVariables();
-        FixRange();
+        GenerateRandomMovementVariables();
         //SetNotMouseControlled();
     }
     private void SetupStartingVariables()
     {
         rb2D = GetComponent<Rigidbody2D>();
         myVehicle = GetComponent<IEntityMover>();
+        myParent = gameObject.GetComponentInParent<IParent>();
+    }
+
+    private void GenerateRandomMovementVariables()
+    {
+        movementPeriod = Random.Range(2, 8);
+    }
+
+    #region Serialization
+    public void OnBeforeSerialize()
+    {
+        FixRange();
+    }
+
+    public void OnAfterDeserialize()
+    {
+
     }
     private void FixRange()
     {
         if (chaseRange < avoidRange)
         {
-            chaseRange = avoidRange;
+            chaseRange = avoidRange + 1;
+        }
+        if (chaseRange < attackRange)
+        {
+            chaseRange = attackRange + 1;
         }
     }
+    #endregion
+
     private void SetNotMouseControlled()
     {
         IPlayerControllable[] components = GetComponentsInChildren<IPlayerControllable>();
@@ -74,8 +103,8 @@ public class ShipController : TeamUpdater
     }
     private Vector2 TranslateToLocalForce(Vector2 globalForce)
     {
-        float entityGlobalAngle = rb2D.gameObject.transform.rotation.eulerAngles.z;
-        Vector2 rotatedVector = HelperMethods.VectorUtils.RotateVector(globalForce, entityGlobalAngle);
+        float shipGlobalAngle = rb2D.gameObject.transform.rotation.eulerAngles.z;
+        Vector2 rotatedVector = HelperMethods.VectorUtils.RotateVector(globalForce, shipGlobalAngle);
         //Debug.Log("Rotate by angle: " + entityGlobalAngle);
         Debug.DrawRay(transform.position, rotatedVector, Color.green, 0.1f);
 
@@ -93,25 +122,21 @@ public class ShipController : TeamUpdater
         List<GameObject> avoidObjects = GetObjectsToAvoid();
         Vector2 avoidVector = CalculateAvoidVector(avoidObjects);
 
-        if (avoidVector.magnitude > 1)
+        Vector2 randomMovement = CalculateRandomMovementVector();
+
+        float chaseLength = chaseVector.magnitude;
+
+        if (chaseLength < 1)
         {
-            return avoidVector.normalized;
+            Vector2 movementVector = chaseVector + (1-chaseLength)* randomMovement;
+            return chaseLength * movementVector + (1 + obstacleAvoidance - chaseLength) * avoidVector.normalized;
         }
-        else
+
+        if (chaseLength > 1)
         {
-            if (avoidVector.magnitude > chaseVector.magnitude)
-            {
-                return avoidVector;
-            }
-            else
-            {
-                if (chaseVector.magnitude > 1)
-                {
-                    chaseVector = chaseVector.normalized;
-                }
-                return chaseVector + avoidVector;
-            }
+            chaseLength = 1;
         }
+        return chaseLength * chaseVector.normalized + (1 + obstacleAvoidance - chaseLength) * avoidVector.normalized;
     }
 
     #region Chasing
@@ -139,22 +164,25 @@ public class ShipController : TeamUpdater
     }
     private Vector2 HandleChaseObject(Vector2 deltaPositionToItem)
     {
-        bool isInAvoidRange = deltaPositionToItem.magnitude < avoidRange;
-        if (isInAvoidRange)
+        bool isAboveAttackRange = deltaPositionToItem.magnitude > attackRange;
+        if (isAboveAttackRange)
         {
-            float multiplier = 1 / deltaPositionToItem.sqrMagnitude;
-            Debug.DrawRay(transform.position, -deltaPositionToItem.normalized * multiplier, Color.red, 0.05f);
-            return -deltaPositionToItem.normalized * multiplier;
-        }
-        else
-        {
+            // Prioritize chasing more, if farther away from the target!
             float multiplier = deltaPositionToItem.sqrMagnitude;
             Debug.DrawRay(transform.position, deltaPositionToItem.normalized * multiplier, Color.blue, 0.05f);
             return deltaPositionToItem;
         }
+        else
+        {
+            // Fall back, if too close to target. Keep distance!
+            float multiplier = 1 / deltaPositionToItem.sqrMagnitude;
+            Debug.DrawRay(transform.position, -deltaPositionToItem.normalized * multiplier, Color.red, 0.05f);
+            return -deltaPositionToItem.normalized * multiplier;
+        }
     }
     #endregion
 
+    #region Obstacle Avoidance
     private Vector2 CalculateAvoidVector(List<GameObject> avoidObjects)
     {
         Vector2 proximityVector = Vector2.zero;
@@ -181,6 +209,7 @@ public class ShipController : TeamUpdater
         }
         else
         {
+            // Multiply by more, if closer to target
             float multiplier = 1 / deltaPositionToItem.sqrMagnitude;
             Debug.DrawRay(transform.position, -deltaPositionToItem * multiplier, Color.yellow, 0.05f);
             return -deltaPositionToItem.normalized * multiplier;
@@ -207,7 +236,6 @@ public class ShipController : TeamUpdater
     private bool IsMyPart(GameObject part)
     {
         IParent partParent = part.GetComponentInParent<IParent>();
-        IParent myParent = gameObject.GetComponentInParent<IParent>();
 
         if (partParent == null || myParent == null)
         {
@@ -215,6 +243,15 @@ public class ShipController : TeamUpdater
         }
         return partParent.GetGameObject() == myParent.GetGameObject();
     }
+    #endregion
+
+    #region Random movement
+    private Vector2 CalculateRandomMovementVector()
+    {
+        float period = (Time.time / movementPeriod) * 2 * Mathf.PI;
+        return transform.right * Mathf.Sin(period);
+    }
+    #endregion
     #endregion
 
     #endregion
